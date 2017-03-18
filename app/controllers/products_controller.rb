3b6d7d38005ab6b3743ecc20d026d5329aae0558
BillW <billwagner102@gmail.com>
2017-03-18 08:11:45 -0700

    5.2 final commit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,6 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    binding.pry
     @price_limit = params[:price_limit].to_i
     @products = Product.where(:price => 0..@price_limit)
     render :json => @products
--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,6 @@ class Product < ApplicationRecord
 end
 
 def self.search(search_term)
-	Product.where("name LIKE ?", "%#{search_term}%")
+	Product.where("name ILIKE ?", "%#{search_term}%")
 end
 
