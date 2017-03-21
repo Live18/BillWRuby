bb307e8fafe5bbd61c1aebdb292ea40f24c71fcb
BillW <billwagner102@gmail.com>
2017-03-21 13:19:44 -0700

    First 5.3 push

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,4 +5,14 @@ class StaticPagesController < ApplicationController
   def landing_page
   	@products = Product.limit(4)  
   end
+
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email, 
+  		:to => 'billwagner102@gmail.com',
+  		:subject => 'New ARC Contact Email from #{@name}',
+  		:body => @message).deliver_now
+  end 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,19 @@
 Rails.application.routes.draw do
   resources :products
+
+  resources :orders, only: [:index, :show, :create, :destroy]
   
   get 'static_pages/about'
 
-  get 'static_pages/contact'
+  get 'static_pages/contact' 
 
   get 'static_pages/index'
+
+  post 'static_pages/thank_you'
+
   root 'static_pages#about'
 
-  resources :orders, only: [:index, :show, :create, :destroy]
+ 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
