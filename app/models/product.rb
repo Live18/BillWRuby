e2b910aa8051003c9f09749e839d5df1c24b2b8f
BillW <billwagner102@gmail.com>
2017-03-20 20:26:36 -0700

    5.2 push

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,6 +3,11 @@ class Product < ApplicationRecord
 end
 
 def self.search(search_term)
-	Product.where("name ILIKE ?", "%#{search_term}%")
+	if Rails.env.development?
+		Product.where("name LIKE ?", "%#{search_term}%")
+	else Rails.env.production?
+		Product.where("name ilike ?", "%#{search_term}%")
+	end
+
 end
 
