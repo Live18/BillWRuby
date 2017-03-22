50f771ae452f885d4f24b870369b2656ec810126
BillW <billwagner102@gmail.com>
2017-03-21 19:50:32 -0700

    5.3 Mailers Update

--- config/environment.rb
+++ config/environment.rb
@@ -11,5 +11,5 @@ ActionMauler::Base.smtp_settings = {
 	:user_name => ENV['SENDGRID_USERNAME'],
 	:password => ENV['SENDGRID_PASSWORD'],
 	:domain => 'heroku.com',
-	"enable_starttls_auto => true"
+	:enable_starttls_auto => true
 }
