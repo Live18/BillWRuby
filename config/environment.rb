5e79dbf85d84c339d7b14bc3cd4b91deb452a2f6
BillW <billwagner102@gmail.com>
2017-03-21 19:46:16 -0700

    5.3 Mailer setup

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMauler::Base.smtp_setting = {
+	:address => 'smtp.sendgrid.net',
+	:port => '587',
+	:authentication => :plain,
+	:user_name => ENV['SENDGRID_USERNAME'],
+	:password => ENV['SENDGRID_PASSWORD'],
+	:domain => 'heroku.com',
+	"enable_starttls_auto => true"
+}
