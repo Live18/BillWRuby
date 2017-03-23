9440d0d953d24e13c4d37d1f044a5d5ad080ae11
BillW <billwagner102@gmail.com>
2017-03-23 08:37:29 -0700

    5.4 commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+  # devise_for :users, :path => '', :path_names => {:sign_in => 'login', :sign_out => 'logout'}
+
+  devise_for :users
   resources :users
   resources :products
 
