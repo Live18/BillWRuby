d0799f0214bf6617b132972f984cb60451d0882c
BillW <billwagner102@gmail.com>
2017-03-26 08:52:44 -0700

    5.5 Fixes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,5 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   rescue_from CanCan::AccessDenied do |exception|
   	redirect_to main_app.root_url, :alert => exception.message
+  end
 end
