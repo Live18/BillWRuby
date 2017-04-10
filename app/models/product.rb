2bdec04439699d75419b44c059e1f68c9591ae3e
BillW <billwagner102@gmail.com>
2017-04-09 18:48:33 -0700

    5.8 Adding Raty

--- app/models/product.rb
+++ app/models/product.rb
@@ -14,6 +14,9 @@ class Product < ApplicationRecord
     comments.rating_desc.first
   end
   def lowest_rating_comment
-    comments.rating_asc.last
+    comments.rating_asc.first
+  end
+  def average_rating
+    comments.average(:rating).to_f
   end
 end
