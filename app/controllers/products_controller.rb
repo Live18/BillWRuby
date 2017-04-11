347b85872462e712fd5eac9d44095b4cc3baed8f
BillW <billwagner102@gmail.com>
2017-04-11 07:36:26 -0700

    5.9 clean up

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,8 +24,7 @@ class ProductsController < ApplicationController
   # GET /products/1
   # GET /products/1.json
   def show
-    @comments = @product.comments.order("created_at DESC")
-      @comments = @product.comments.paginate(page: params[:page], per_page: 5)
+    @comments = @product.comments.order("created_at DESC").paginate(page: params[:page], per_page: 5)
   end
 
   # GET /products/new
