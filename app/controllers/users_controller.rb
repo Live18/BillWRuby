adb211763305d2243a55805d845dbefd3eec68e8
BillW <billwagner102@gmail.com>
2017-04-18 16:46:46 -0700

    User controller error fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,4 @@
-usclass UsersController < ApplicationController
+class UsersController < ApplicationController
   before_action :set_user, only: [:show, :edit, :update, :destroy]
   before_filter :authenticate_user!
   load_and_authorize_resource
