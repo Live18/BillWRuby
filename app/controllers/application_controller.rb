4acd902de10a1443f1ab97ebc524c72b44df6738
Zane <i.zane.am.robot@gmail.com>
2017-05-01 11:53:52 -0400

    added is_admin booleatn method to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,10 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
   	redirect_to main_app.root_url, :alert => exception.message
   end
+
+  def is_admin?
+  	unless user_signed_in? && current_user.admin?
+  		redirect_to root_url, notice: 'Sorry, you do not have permission to do that.'
+  	end
+  end
 end
