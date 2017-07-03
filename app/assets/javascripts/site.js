92163623ee2607f2690cbfd392796c72a146bee4
BillW <billwagner102@gmail.com>
2017-07-03 09:10:01 -0700

    6.10 Ajax updates

--- app/assets/javascripts/site.js
+++ app/assets/javascripts/site.js
@@ -1,10 +1,11 @@
-$(document).on('turbolinks:load', function(){
+$(document).on('turbolinks:load ajax:complete', function(){
 	$('.rating').raty({path: '/assets', scoreName: 'comment[rating]'});
 	$('.rated').raty({path: '/assets',
 		readOnly: true,
 		score: function(){
 			return $(this).attr('data-score');
 		}
+
 	});
 	/* elevate zoom plugin*/
     $('.img-zoom').elevateZoom({
